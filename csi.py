0eaa78e0efaa3ad7acdec9a3972ee12bd14897b9
John Zhang <john.jiabin.zhang@gmail.com>
2016-06-14 15:10:13 +1000

    Python3

--- csi.py
+++ csi.py
@@ -4,27 +4,27 @@
 #	Author: John Zhang
 """
 
-_CSI = '\033[';
+_CSI = '\033['
 
 # Cursor controls
 
 
-def _CSI_CUU(N): return "%s%d%s" %  (_CSI, N, "A"); # Cursor Up (default 1)
-def _CSI_CUD(N): return "%s%d%s" %  (_CSI, N, "B"); # Cursor Down (default 1)
-def _CSI_CUF(N): return "%s%d%s" %  (_CSI, N, "C"); # Cursor Forward (default 1)
-def _CSI_CUB(N): return "%s%d%s" %  (_CSI, N, "D"); # Cursor Back (default 1)
-def _CSI_CNL(N): return "%s%d%s" %  (_CSI, N, "E"); # Cursor Next Line: Move cursor to beginning of N (default 1) lines down.
-def _CSI_CPL(N): return "%s%d%s" %  (_CSI, N, "F"); # Cursor Previous Line: Move cursor to beginning of N (default 1) lines up.
-def _CSI_CHA(N): return "%s%d%s" %  (_CSI, N, "G"); # Cursor Horizontal Absolute: Move to column n
-def _CSI_CUP(N,M): return "%s%d%s" %  (_CSI, N, "H"); # C" //Moves the cursor to row n, column m. The values are 1-based, and default to 1 (top left corner) if omitted. A sequence such as _CSI ; #5H is a synonym for _CSI 1; #5H as well as _CSI 17; #H is the same as _CSI 17H and _CSI 17; #1H
-def _CSI_ED(N): return "%s%d%s" %  (_CSI, N, "J"); # Clears part of the screen. If n is zero (or missing), clear from cursor to end of screen. If n is one, clear from cursor to beginning of the screen. If n is two, clear entire screen (and moves cursor to upper left on MS-DOS ANSI.SYS).
-def _CSI_EL(N): return "%s%d%s" %  (_CSI, N, "K"); # Crases part of the line. If n is zero (or missing), clear from cursor to the end of the line. If n is one, clear from cursor to beginning of the line. If n is two, clear entire line. Cursor position does not change.
-def _CSI_SU(N): return "%s%d%s" %  (_CSI, N, "S"); # Ccroll whole page up by n (default 1) lines. New lines are added at the bottom. (not ANSI.SYS)
-def _CSI_SD(N): return "%s%d%s" %  (_CSI, N, "T"); # Ccroll whole page down by n (default 1) lines. New lines are added at the top. (not ANSI.SYS)
-def _CSI_HVP(N,M): return _CSI_CUP(N, M);
+def _CSI_CUU(N): return "%s%d%s" %  (_CSI, N, "A") # Cursor Up (default 1)
+def _CSI_CUD(N): return "%s%d%s" %  (_CSI, N, "B") # Cursor Down (default 1)
+def _CSI_CUF(N): return "%s%d%s" %  (_CSI, N, "C") # Cursor Forward (default 1)
+def _CSI_CUB(N): return "%s%d%s" %  (_CSI, N, "D") # Cursor Back (default 1)
+def _CSI_CNL(N): return "%s%d%s" %  (_CSI, N, "E") # Cursor Next Line: Move cursor to beginning of N (default 1) lines down.
+def _CSI_CPL(N): return "%s%d%s" %  (_CSI, N, "F") # Cursor Previous Line: Move cursor to beginning of N (default 1) lines up.
+def _CSI_CHA(N): return "%s%d%s" %  (_CSI, N, "G") # Cursor Horizontal Absolute: Move to column n
+def _CSI_CUP(N,M): return "%s%d%s" %  (_CSI, N, "H") # C" //Moves the cursor to row n, column m. The values are 1-based, and default to 1 (top left corner) if omitted. A sequence such as _CSI  #5H is a synonym for _CSI 1 #5H as well as _CSI 17 #H is the same as _CSI 17H and _CSI 17 #1H
+def _CSI_ED(N): return "%s%d%s" %  (_CSI, N, "J") # Clears part of the screen. If n is zero (or missing), clear from cursor to end of screen. If n is one, clear from cursor to beginning of the screen. If n is two, clear entire screen (and moves cursor to upper left on MS-DOS ANSI.SYS).
+def _CSI_EL(N): return "%s%d%s" %  (_CSI, N, "K") # Crases part of the line. If n is zero (or missing), clear from cursor to the end of the line. If n is one, clear from cursor to beginning of the line. If n is two, clear entire line. Cursor position does not change.
+def _CSI_SU(N): return "%s%d%s" %  (_CSI, N, "S") # Ccroll whole page up by n (default 1) lines. New lines are added at the bottom. (not ANSI.SYS)
+def _CSI_SD(N): return "%s%d%s" %  (_CSI, N, "T") # Ccroll whole page down by n (default 1) lines. New lines are added at the top. (not ANSI.SYS)
+def _CSI_HVP(N,M): return _CSI_CUP(N, M)
 
 
-def _CSI_SGR(N): return "%s%sm" % (_CSI, N);
+def _CSI_SGR(N): return "%s%sm" % (_CSI, N)
 
 _CSI_SGR_PARAM_RESET =									"0"	#All attributes off
 _CSI_SGR_PARAM_BOLD =									"1"	
@@ -33,7 +33,7 @@ _CSI_SGR_PARAM_ITALIC_ON =								"3"	#Not widely supported, sometimes treated a
 _CSI_SGR_PARAM_UNDERLINE_SINGLE =						"4"
 _CSI_SGR_PARAM_BLINK_SLOW =							"5"
 _CSI_SGR_PARAM_BLINK_RAPID =							"6"
-_CSI_SGR_PARAM_IMAGE_NEGATIVE =						"7"	#Inverse or reverse; swap foreground and background.
+_CSI_SGR_PARAM_IMAGE_NEGATIVE =						"7"	#Inverse or reverse swap foreground and background.
 _CSI_SGR_PARAM_FONT_PRIME =							"10"
 _CSI_SGR_PARAM_FONT_ALT_1 =							"11"
 _CSI_SGR_PARAM_FONT_ALT_2 =							"12"
@@ -68,7 +68,7 @@ _CSI_SGR_PARAM_BG_CLR_CYAN =							"46"
 _CSI_SGR_PARAM_BG_CLR_WHITE =							"47"
 _CSI_SGR_PARAM_BG_CLR_DEFAULT =						"49"
 
-_CSI_DSR = _CSI + "6" + "n" #Reports the cursor position to the application as (as though typed at the keyboard) ESC[n;mR, where n is the row and m is the column. (May not work on MS-DOS.)
+_CSI_DSR = _CSI + "6" + "n" #Reports the cursor position to the application as (as though typed at the keyboard) ESC[nmR, where n is the row and m is the column. (May not work on MS-DOS.)
 _CSI_SCP = _CSI + "s" #Save cursor position
 _CSI_RCP = _CSI + "u" #Restores the cursor position
 _CSI_CUHIDE = _CSI + "?25!"	#Hides the cursor (trailing character is lowercase L.)
@@ -86,14 +86,14 @@ _CSI_UTIL_CLR_MAGENTA =	_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_MAGENTA)
 _CSI_UTIL_CLR_CYAN =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_CYAN)
 _CSI_UTIL_CLR_WHITE =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_WHITE)
 
-_CSI_UTIL_CLR_BLACK_BOLD = 		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_BLACK + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_RED_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_RED + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_GREEN_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_GREEN + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_YELLOW_BOLD =	_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_YELLOW + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_BLUE_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_BLUE + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_MAGENTA_BOLD =	_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_MAGENTA + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_CYAN_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_CYAN + ';' + _CSI_SGR_PARAM_BOLD)
-_CSI_UTIL_CLR_WHITE_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_WHITE + ';' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_BLACK_BOLD = 		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_BLACK + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_RED_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_RED + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_GREEN_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_GREEN + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_YELLOW_BOLD =	_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_YELLOW + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_BLUE_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_BLUE + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_MAGENTA_BOLD =	_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_MAGENTA + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_CYAN_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_CYAN + '' + _CSI_SGR_PARAM_BOLD)
+_CSI_UTIL_CLR_WHITE_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_TEXT_CLR_WHITE + '' + _CSI_SGR_PARAM_BOLD)
 _CSI_UTIL_BOLD =		_CSI_SGR(_CSI_SGR_PARAM_BOLD)
 _CSI_UTIL_RESET =				_CSI_SGR(_CSI_SGR_PARAM_RESET)
 
@@ -102,7 +102,7 @@ def _CSI_STR_RED(str): return "%s%s%s" % (    _CSI_UTIL_CLR_RED, str, _CSI_UTIL_
 def _CSI_STR_GREEN(str): return "%s%s%s" % (  _CSI_UTIL_CLR_GREEN, str, _CSI_UTIL_RESET )
 def _CSI_STR_YELLOW(str): return "%s%s%s" % ( _CSI_UTIL_CLR_YELLOW, str, _CSI_UTIL_RESET )
 def _CSI_STR_BLUE(str): return "%s%s%s" % (   _CSI_UTIL_CLR_BLUE, str, _CSI_UTIL_RESET )
-def _CSI_STR_MAGNET(str): return "%s%s%s" % ( _CSI_UTIL_CLR_MAGENT, str, _CSI_UTIL_RESET )
+def _CSI_STR_MAGNET(str): return "%s%s%s" % ( _CSI_UTIL_CLR_MAGENTA, str, _CSI_UTIL_RESET )
 def _CSI_STR_CYAN(str): return "%s%s%s" % (   _CSI_UTIL_CLR_CYAN, str, _CSI_UTIL_RESET )
 def _CSI_STR_WHITE(str): return "%s%s%s" % (  _CSI_UTIL_CLR_WHITE, str, _CSI_UTIL_RESET )
 
@@ -111,7 +111,7 @@ def _CSI_STR_RED_BOLD(str): return "%s%s%s" % (    _CSI_UTIL_CLR_RED_BOLD, str, 
 def _CSI_STR_GREEN_BOLD(str): return "%s%s%s" % (  _CSI_UTIL_CLR_GREEN_BOLD, str, _CSI_UTIL_RESET )
 def _CSI_STR_YELLOW_BOLD(str): return "%s%s%s" % ( _CSI_UTIL_CLR_YELLOW_BOLD, str, _CSI_UTIL_RESET )
 def _CSI_STR_BLUE_BOLD(str): return "%s%s%s" % (   _CSI_UTIL_CLR_BLUE_BOLD, str, _CSI_UTIL_RESET )
-def _CSI_STR_MAGNET_BOLD(str): return "%s%s%s" % ( _CSI_UTIL_CLR_MAGENT_BOLD, str, _CSI_UTIL_RESET )
+def _CSI_STR_MAGNET_BOLD(str): return "%s%s%s" % ( _CSI_UTIL_CLR_MAGENTA_BOLD, str, _CSI_UTIL_RESET )
 def _CSI_STR_CYAN_BOLD(str): return "%s%s%s" % (   _CSI_UTIL_CLR_CYAN_BOLD, str, _CSI_UTIL_RESET )
 def _CSI_STR_WHITE_BOLD(str): return "%s%s%s" % (  _CSI_UTIL_CLR_WHITE_BOLD, str, _CSI_UTIL_RESET )
 
@@ -136,15 +136,7 @@ cyan_bold =    _CSI_UTIL_CLR_CYAN_BOLD
 white_bold =   _CSI_UTIL_CLR_WHITE_BOLD
 
 def clrprint(s, clr):
-  print "%s%s%s" % (clr, s, _CSI_UTIL_RESET);
+    print("%s%s%s" % (clr, s, _CSI_UTIL_RESET))
 
 def clrstr(s, clr):
-  return "%s%s%s" % (clr, s, _CSI_UTIL_RESET);
-
-###########################################################################################################
-def test():
-  clrprint("Red", red);
-  print clrstr("Blue", blue);
-
-if __name__ == "__main__":
-  test();
+    return "%s%s%s" % (clr, s, _CSI_UTIL_RESET)
